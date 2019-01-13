5af6efb4aba85436fbf9166095c14e6ee1539dbf
Fahari <karonjekevin67@gmail.com>
2019-01-13 09:49:44 +0300

    add an update of quotes into the quotess array inside the app.component.ts file, while adding view code in the app.component.html

--- quotes/src/app/app.component.ts
+++ quotes/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Quotes} from './quotes'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quotes';
+  quotess=[
+    new Quotes(1,'Most people are other people. Their thoughts are someone else’s opinions, their lives a mimicry, their passions a quotation.'),
+    new Quotes(2,'To forgive is to set a prisoner free and discover that the prisoner was you.'),
+    new Quotes(3,'Money and success don’t change people; they merely amplify what is already there.'),
+    new Quotes(4,'Everyone thinks of changing the world, but no one thinks of changing himself.'),
+  ]
 }
